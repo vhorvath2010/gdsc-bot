535ec4f6c36b412262b1538273380a9001a20762
Vince <vhorvath2010@gmail.com>
2022-03-08 17:58:23 -0800

    Added basic html page upon regular get request

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+});
 
 app.post('/', (req, res) => {
     
